07af2428384706952ec73bba8673951df317eca1
Jeremy Ellis <keyfreemusic@gmail.com>
2020-10-23 09:12:39 -0700

    Update camera.js

--- public/camera.js
+++ public/camera.js
@@ -30,6 +30,7 @@ import {Skeleton, facePartName2Index} from './illustrationGen/skeleton';
 import {FileUtils} from './utils/fileUtils';
 
 
+
 import * as girlSVG from './resources/illustration/girl.svg';
 import * as boySVG from './resources/illustration/boy.svg';
 import * as abstractSVG from './resources/illustration/abstract.svg';
